e606515afa66a6ddbae057295e1f721c580f9e86
Prasanthimsc <108698203+Prasanthimsc@users.noreply.github.com>
2022-11-29 10:05:40 -0800

    Update baseClass.java

--- src/test/java/StepDefs/baseClass.java
+++ src/test/java/StepDefs/baseClass.java
@@ -3,6 +3,10 @@ package StepDefs;
 import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.ResourceBundle;
+
+import Page_Object.Login_Page;
+import Page_Object.RegisterPage
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.RandomStringUtils;
@@ -14,6 +18,13 @@ import org.openqa.selenium.WebDriver;
 import lmsPageObject.addBatchPageObject;
 import lmsPageObject.batchPageObject;
 import lmsPageObject.editBatchPageObject;
+import lmsPageObject.Login_Page;
+import lmsPageObject.Logout_Page;
+import lmsPageObject.RegisterPage;
+import lmsPageObject.ProgramPage;
+import lmsPageObject.User_Page;
+import lmsPageObject.Attendance_page;
+import lmsPageObject.Assignment_page;
 
 public class baseClass {
 	
@@ -23,6 +34,20 @@ public class baseClass {
 	public editBatchPageObject ebpo;
 	public static Logger logger;
 	public Properties configprop;
+	public static WebDriver driver;
+	public static RegisterPage rp;
+	public static Login_Page lp;
+	public static Logout_Page;
+	public static ProgramPage pp;
+	public static User_Page up;
+    public Attendance_page ap;
+    public User_page up;
+    public AssignmentPage Asp;
+    
+        
+	static {
+		driver = getDriver();
+	}
 	
 	//public LoginPage lp;
 	
