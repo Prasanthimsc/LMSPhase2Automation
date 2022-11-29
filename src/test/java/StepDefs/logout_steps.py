cbc4633fff07217e9ec2cbeb2e65e5977bd351b2
gayathrireddyp <118940396+gayathrireddyp@users.noreply.github.com>
2022-11-29 15:53:01 -0600

    Add files via upload

---
+++ src/test/java/StepDefs/logout_steps.py
@@ -0,0 +1,15 @@
+from behave import *
+from selenium import webdriver
+@given('Admin/User/Staff Logged on to LMS website')
+def home_page(context):
+
+
+
+@when('Admin/User/Staff  Clicks on LogOut button')
+def logout(context):
+
+
+@then('Admin/User/Staff should be navigated to Login Screen')
+def navigation(context):
+
+
